47c0025d0de0c53d9620a0ec26bedfa506d3227b
rohil1418 <rohilk592@gmail.com>
2026-07-12 21:05:00 +0530

    Initial WanderLust project setup

---
+++ models/listing.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+        url: {
+            type: String,
+            default: "https://img.magnific.com/premium-photo/random-image_590832-1108.jpg",
+        },
+    },
+
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
