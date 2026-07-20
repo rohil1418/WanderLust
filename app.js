95799e07f27af0a528fd74defd42cf927580d929
rohil1418 <rohilk592@gmail.com>
2026-07-20 23:26:57 +0530

    Added review functionality and updated listing schema

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema , reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -37,115 +36,10 @@ app.get("/", (req, res) => {
     res.send("Hi i am Rohil");
 });
 
-const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (!req.body || !req.body.listing) {
-        throw new ExpressError(400, "listing is required");
-    }
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }
-    next();
-}
 
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
+app.use("/listings" , listings);
+app.use("/listings/:id/reviews" , reviews);
 
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }
-    next();
-}
-
-app.get("/listings", wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-}));
-
-app.get("/listings/new", (req, res) => {
-    {
-        res.render("listings/new.ejs")
-    }
-})
-
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", { listing });
-}));
-
-app.post("/listings", validateListing,
-    wrapAsync(async (req, res, next) => {
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    })
-);
-
-app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-}));
-
-// Update route
-
-app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-        throw new ExpressError(400, "Send valid data for listing")
-    }
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect("/listings");
-}));
-
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview._id);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-})
-);
-
-//Delete review route
-app.delete("/listings/:id/reviews/:reviewId",
-    wrapAsync(async (req , res) =>{
-        let { id , reviewId } = req.params;
-        await Listing.findByIdAndUpdate( id , {$pull: {reviews: reviewId } });
-        await Review.findByIdAndDelete(reviewId);
-
-        res.redirect(`/listings/${id}`);
-    })
-);
-
-// app.get("/testListing", async (req , res) => {
-//     let sampleListing = new Listing ({
-//         title : "My new villa",
-//         description : "By the beach",
-//         price : 1200,
-//         location : "Jaipur , Rajasthan",
-//         country : "India",
-//     });
-
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("successful testing");
-// });
 
 app.all("*splat", (req, res, next) => {
     next(new ExpressError(404, "Page not found"));
--- classroom/server.js
+++ classroom/server.js
@@ -2,14 +2,50 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const session = require("express-session");
 
-app.use("/users" , users);
-app.use("/posts" , posts);
 
-app.get("/" , (req , res) => {
-    res.send("Hi i am RK");
-});
+app.use(session({secret: 'mrsupersecretstring'}));
+
+app.get("/test" , (req , res) => {
+    res.send("test seccessfull")
+})
+
 
 app.listen(3000 , () => {
     console.log("Server is listening");
 });
+
+
+
+// const cookieParser = require("cookie-parser");
+
+// app.use(cookieParser("secretcode"));
+
+// app.get("/getsignedcookie" , (req , res) => {
+//     res.cookie("Rohil" , "Khan" , {signed : true});
+//     res.send("signed cookie sent");
+// });
+
+// app.get("/verify" , (req , res) => {
+//     console.log(req.signedCookies);
+//     res.send("verified");
+// });
+
+// app.get("/getcookies" , (req , res) => {
+//     res.cookie("greet" , "hello");
+//     res.send("sent you some cookies");
+// });
+
+// app.use("/users" , users);
+// app.use("/posts" , posts);
+
+// app.get("/greet" , (req , res) => {
+//     let { name = "anonymous"} = req.cookies;
+//     res.send(`HI, ${name}`);
+// });
+
+// app.get("/" , (req , res) => {
+//     console.dir(req.cookies);
+//     res.send("Hi i am RK");
+// });
