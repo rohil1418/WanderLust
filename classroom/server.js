2a864e028609e08daaaf07036726c6aae22834a5
rohil1418 <rohilk592@gmail.com>
2026-07-21 23:34:33 +0530

    Add flash messages and EJS view configuration

--- classroom/server.js
+++ classroom/server.js
@@ -3,17 +3,47 @@ const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 
-app.use(session({secret: 'mrsupersecretstring'}));
+const sessionOptions = {
+    secret: "mysupersecretstring", 
+    resave : false,
+    saveUninitialized: true
+}
 
-app.get("/test" , (req , res) => {
-    res.send("test seccessfull")
-})
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.get("/register" , (req , res) => {
+    let {name = "anonymous"} = req.query;
+    req.session.name = name ;
+    req.flash("success", "user registered successfully!");
+    res.redirect("/hello");
+});
+
+app.get("/hello" , (req , res) =>{
+   res.render("page.ejs" , {name: req.session.name, msg:req.flash("success") });
+});
+
+
+
+// app.get("/reqcount" , (req , res) => {
+//     req.session.count = 1;
+//     res.send(`You sent a request ${req.session.count} times`);
+// });
+
+// app.get("/test" , (req , res) => {
+//     res.send("test seccessfull")
+// })
 
 
 app.listen(3000 , () => {
-    console.log("Server is listening");
+    console.log("Server is listening 3000");
 });
 
 
