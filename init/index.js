4af47b2d855cb85cb8d43d1f117546611f47083c
rohil1418 <rohilk592@gmail.com>
2026-07-24 23:41:00 +0530

    chore(seed): update database initialization data

--- init/index.js
+++ init/index.js
@@ -19,6 +19,7 @@ async function main(){
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj , owner:"6a624e9801e6b1e124119aa7"}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
